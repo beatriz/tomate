5d5defd7dc824210450baf1bb79f5f94760e639e
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2018-04-15 11:52:13 +0100

    setup eslint

--- javascript/pomodoro.js
+++ javascript/pomodoro.js
@@ -1,112 +1,112 @@
-var timerID,
- seconds,
- numBreaks,
- nextType,
- notifTitle, notifText,
- paused
+/* global $ */
+var timerID;
+var seconds;
+var nextType;
+var notifTitle;
+var notifText;
+var paused;
 
-numBreaks = 1;
+var numBreaks = 1;
+
+function runTimer(s) {
+  timer(s); // eslint-disable-line
+  timerID = setInterval('timer(s)', 1000); // eslint-disable-line
+}
+
+function notify(presentType, title, text) {
+  if (presentType === 'p') {
+    $.growl.error({ title: title, message: text });
+  } else {
+    $.growl.notice({ title: title, message: text });
+  }
+// $.growl({ title: "Growl", message: "The kitten is awake!" });
+}
+
 function pomodoro(presentType) {
-	clearInterval(timerID);
-	if(presentType == "p") {
-		pomodoroMinutes = document.getElementById('ptime').value;
-		document.title = "POMODORO";
-		color = "black";
-		if((numBreaks % 4) == 0) {
-			nextType = 'lb';
-		} else {
-			nextType = 'sb';
-		}
-		notifTitle = "Pomodoro time!";
-		notifText = "Time for a pomodoro!";
-		paused = 0;
-	} else if(presentType == "sb")
-	{
-		document.title = "POMODORO - Short break";
-		pomodoroMinutes = document.getElementById('sbtime').value;
-		numBreaks++;
-		nextType = 'p';
-		color = "#008000";
-		notifTitle = "Break time!";
-		notifText = "Time for a short break!";
-	} else {
-		document.title = "POMODORO - Long break";
-		pomodoroMinutes = document.getElementById('lbtime').value;
-		numBreaks++;
-		nextType = 'p';
-		color = "#00FF00";
-		notifTitle = "Break time!";
-		notifText = "Time for a long break!";
-	}
-	document.getElementById('minute').style.color = color;
-	document.getElementById('second').style.color = color;
-	seconds = pomodoroMinutes * 60;
-	runTimer(seconds);
-	notify(presentType, notifTitle, notifText);
+  var pomodoroMinutes;
+  var color;
+  clearInterval(timerID);
+  if (presentType === 'p') {
+    pomodoroMinutes = document.getElementById('ptime').value;
+    document.title = 'POMODORO';
+    color = 'black';
+    if ((numBreaks % 4) === 0) {
+      nextType = 'lb';
+    } else {
+      nextType = 'sb';
+    }
+    notifTitle = 'Pomodoro time!';
+    notifText = 'Time for a pomodoro!';
+    paused = 0;
+  } else if (presentType === 'sb') {
+    document.title = 'POMODORO - Short break';
+    pomodoroMinutes = document.getElementById('sbtime').value;
+    numBreaks += 1;
+    nextType = 'p';
+    color = '#008000';
+    notifTitle = 'Break time!';
+    notifText = 'Time for a short break!';
+  } else {
+    document.title = 'POMODORO - Long break';
+    pomodoroMinutes = document.getElementById('lbtime').value;
+    numBreaks += 1;
+    nextType = 'p';
+    color = '#00FF00';
+    notifTitle = 'Break time!';
+    notifText = 'Time for a long break!';
+  }
+  document.getElementById('minute').style.color = color;
+  document.getElementById('second').style.color = color;
+  seconds = pomodoroMinutes * 60;
+  runTimer(seconds);
+  notify(presentType, notifTitle, notifText);
 }
 
-function runTimer(seconds) {
-	timer(seconds);
-	timerID = setInterval('timer(seconds)', 1000);
-}
-function pause() {
-	if(paused)
-	{
-		paused = 0;
-		runTimer(seconds);
-		text = 'Pause';
-		
-	}
-	else {
-		paused = 1;
-		clearInterval(timerID);
-		text = 'Continue';
-	}
-	document.getElementById('stop').value = text;
-	
+function pause() { // eslint-disable-line
+  var text;
+  if (paused) {
+    paused = 0;
+    runTimer(seconds);
+    text = 'Pause';
+  } else {
+    paused = 1;
+    clearInterval(timerID);
+    text = 'Continue';
+  }
+  document.getElementById('stop').value = text;
 }
 
-function timer(){
-	var minutes = Math.round((seconds - 30)/60);
-	if(minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	var remainingSeconds = seconds % 60;
-	if(remainingSeconds < 10) {
-		remainingSeconds = "0" + remainingSeconds;
-	}
-	document.getElementById('minute').innerHTML = minutes;
-	document.getElementById('second').innerHTML = remainingSeconds;
-	if(seconds > 0) {
-		seconds--;		
-	}
-	else {
-		clearInterval(timerID);
-		pomodoro(nextType);
-		playSound("bell");
-	}
+function playSound(soundObj) {
+  var sound = document.getElementById(soundObj);
+  sound.play();
 }
 
-function reset(){
-	clearInterval(timerID);
-	document.getElementById('ptime').value = 25;
-	document.getElementById('sbtime').value = 5;
-	document.getElementById('lbtime').value = 15;
-	document.getElementById('minute').innerHTML = 25;
-	document.getElementById('second').innerHTML = '00';
+function timer() {
+  var remainingSeconds;
+  var minutes = Math.round((seconds - 30) / 60);
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  remainingSeconds = seconds % 60;
+  if (remainingSeconds < 10) {
+    remainingSeconds = '0' + remainingSeconds;
+  }
+  document.getElementById('minute').innerHTML = minutes;
+  document.getElementById('second').innerHTML = remainingSeconds;
+  if (seconds > 0) {
+    seconds -= 1;
+  } else {
+    clearInterval(timerID);
+    pomodoro(nextType);
+    playSound('bell');
+  }
 }
 
-function playSound(soundObj){
-	var sound = document.getElementById(soundObj);
-	sound.play();
-}
-function notify(presentType, notifTitle, notifText) {
-	if(presentType == 'p')
-	{
-		$.growl.error({title: notifTitle, message: notifText});
-	}
-	else {
-		$.growl.notice({title: notifTitle, message: notifText});
-	}
-//$.growl({ title: "Growl", message: "The kitten is awake!" });
+function reset() { // eslint-disable-line
+  clearInterval(timerID);
+  document.getElementById('ptime').value = 25;
+  document.getElementById('sbtime').value = 5;
+  document.getElementById('lbtime').value = 15;
+  document.getElementById('minute').innerHTML = 25;
+  document.getElementById('second').innerHTML = '00';
 }
